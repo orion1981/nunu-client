835206a29bf29ff4ec14a316e4ec0f91f788b3fe
orion1981 <cstopek@hotmail.com>
2018-01-19 16:04:05 -0500

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 class App extends Component {
   render() {
