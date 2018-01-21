3f26770726be8c28215a5ebb9d8f1a0558d38f0f
orion1981 <cstopek@hotmail.com>
2018-01-21 14:45:35 -0500

    set up react-router-dom and routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Login from './containers/Login';
+import LocationList from './containers/LocationList';
+import PackagingList from './containers/PackagingList';
+import ProductionBreakdown from './containers/ProductionBreakdown';
+import SingleLocation from './containers/SingleLocation';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Switch>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+              <Route path="/Login" render={() =><Login />}/>
+              <Route path="/LocationList" render={() =><LocationList />}/>
+              <Route path="/PackagingList" render={() =><PackagingList />}/>
+              <Route path="/ProductionBreakdown" render={() =><ProductionBreakdown />}/>
+              <Route path="/Single Location" render={() =><SingleLocation />}/>
+          </div>
+        <Switch/>
+      <Router/>
     );
   }
 }
