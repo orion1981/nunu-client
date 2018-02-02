fb8c0d72be2174bcf948a3e3353e4c36abb32384
orion1981 <cstopek@hotmail.com>
2018-02-02 14:49:01 -0500

    verifying git on new machine

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -4,12 +4,22 @@ export default class Login extends React.Component {
   constructor(){
     super()
 
+    this.state = {
+      password: ''
+    }
   }
 
   render(){
     return(
       <div>
         <h1>login</h1>
+        <Input
+            secureTextEntry
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+            />
       </div>
     )
   }
