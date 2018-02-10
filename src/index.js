7a4a8ca937e75b009729ad623b9352bccfb80415
orion1981 <cstopek@hotmail.com>
2018-02-10 12:59:47 -0500

    added store to app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
 import './index.css';
+import todoApp from './reducers'
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 
+let store = createStore(todoApp)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store=(store)>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
